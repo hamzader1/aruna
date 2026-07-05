92a27566d23df5270f3ecd98759990b721d0158a
Hamza <official.hamzaderaoui@gmail.com>
2026-07-05 13:20:29 +0100

    Build arena layouts from Rust types

--- arena/src/layout.rs
+++ arena/src/layout.rs
@@ -2,3 +2,12 @@ pub struct ArenaLayout {
     size: usize,
     align: usize,
 }
+
+impl ArenaLayout {
+    pub fn new<T>() -> Self {
+        Self {
+            size: std::mem::size_of::<T>(),
+            align: std::mem::align_of::<T>(),
+        }
+    }
+}
