813cecd8e6d1f55047cb7941e632d15c6de77ebb
Hamza <official.hamzaderaoui@gmail.com>
2026-07-05 13:20:41 +0100

    Build arena layouts from explicit size and alignment

--- arena/src/layout.rs
+++ arena/src/layout.rs
@@ -10,4 +10,13 @@ impl ArenaLayout {
             align: std::mem::align_of::<T>(),
         }
     }
+
+    pub fn from_size_align(size: usize, align: usize) -> std::result::Result<Self, ()> {
+        unsafe {
+            Ok(Self {
+                size,
+                align: std::mem::transmute(align),
+            })
+        }
+    }
 }
